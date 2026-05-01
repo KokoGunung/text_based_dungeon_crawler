6ea09823b65beff82f1d703543fb611575be9782
KokoGunung <girindrakoko@gmail.com>
2026-05-01 20:04:58 +0700

    Main menu

--- main.cpp
+++ main.cpp
@@ -1,9 +1,53 @@
 #include <iostream>
+#include <conio.h>
 using namespace std;
 
+bool main_menu(){
+    //menu
+    int menu;
+    cout<<"=================DUNGEON CRAWLER================\n";
+    cout<<"1. Main\n";
+    cout<<"2. Keluar\n";
+    cout<<": ";
+    cin>>menu;
+    if(menu == 2){
+        return false;
+    }
+    system("cls");
+    
+    //save file
+    cout<<"--------------Pilih save file anda--------------\n";
+    cout<<"1. List save file disini\n";
+    cout<<"2. Kembali\n";
+    cout<<": ";
+    cin>>menu;
+    if(menu == 2){
+        system("cls");
+        return true;
+    }
 
+    //temp
+    if(menu == 1){
+        system("cls");
+        cout<<"Blm ada apa apa njir\n";
+        cout<<"tekan enter untuk lanjut\n";
+        getche();
+        system("cls");
+        return true;
+    }
+    return true;
+}
+
+bool game(){
+    if(!main_menu()){
+        return false;
+    }
+    return true;
+}
 
 int main(){
+    //game loop
+    while(game()){
 
-
+    }
 }
